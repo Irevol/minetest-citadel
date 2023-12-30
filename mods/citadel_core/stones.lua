97f3df38714cc6f6b188fe121ad33a8491f9eb16
Aaron Suen <warr1024@gmail.com>
2023-12-30 00:50:06 -0500

    More dramatic unbarring effects
    The stone of unbarring doesn't just magick the
    door into oblivion, it shatters it into tiny
    fragments.
    
    The stone also operates on an entire connected
    bar construct at once instead of just one vertical
    section at a time.

--- mods/citadel_core/stones.lua
+++ mods/citadel_core/stones.lua
@@ -69,44 +69,76 @@ minetest.register_node(cc.."backward_stone_node", {
 	drop = cc.."backward_stone"
 })
 --unlock stone
+
+local barpref = "xpanes:bar"
+local function unbar(pos)
+	local node = minetest.get_node(pos)
+	if node.name:sub(1, #barpref) ~= barpref then return end
+	minetest.remove_node(pos)
+
+	-- breaking bar effects
+	minetest.add_particlespawner({
+		amount = 50,
+		time = 0.05,
+		collisiondetection = true,
+		collision_removal = false,
+		object_collision = false,
+		vertical = false,
+		node = {name = node.name},
+		minpos = {x=pos.x-0.5, y=pos.y-0.5, z=pos.z-0.5},
+		maxpos = {x=pos.x+0.5, y=pos.y+0.5, z=pos.z+0.5},
+		minvel = {x=-2, y=0.1, z=-2},
+		maxvel = {x=2, y=1, z=2},
+		minacc = {x = 0, y = -9.81, z = 0},
+		maxacc = {x = 0, y = -9.81, z = 0},
+		minsize = 0.25,
+		maxsize = 1,
+		minexptime = 0.5,
+		maxexptime = 1
+	})
+	minetest.sound_play("unbarring", {
+		pos = pos,
+		pitch = math.random() / 10 + 0.95,
+		time_start = math.random() / 5
+	}, true)
+
+	-- magic sparkles
+	minetest.add_particlespawner({
+		amount = 15,
+		time = 3,
+		collisiondetection = false,
+		collision_removal = false,
+		object_collision = false,
+		vertical = false,
+		texture = "blue_particle.png",
+		glow = 10,
+		minpos = {x=pos.x-0.5, y=pos.y-0.5, z=pos.z-0.5},
+		maxpos = {x=pos.x+0.5, y=pos.y+0.5, z=pos.z+0.5},
+		minvel = {x=0, y=0.1, z=0},
+		maxvel = {x=0, y=1, z=0},
+	})
+
+	unbar(vector.offset(pos, 0, 1, 0))
+	unbar(vector.offset(pos, 0, -1, 0))
+	unbar(vector.offset(pos, 1, 0, 0))
+	unbar(vector.offset(pos, -1, 0, 0))
+	unbar(vector.offset(pos, 0, 0, 1))
+	unbar(vector.offset(pos, 0, 0, -1))
+	return true
+end
+
 minetest.register_craftitem(cc.."unlock_stone", {
 	description = "Stone of Unbarring",
 	inventory_image = "small_stone.png^(unlock.png^[colorize:black)",
 	stack_max = 1,
 	on_use = function(itemstack, user, pointed_thing)
-		if pointed_thing.type == "node" then
-			--minetest.chat_send_all(minetest.get_node(pointed_thing.under).name)
-			if pointed_thing.type == "node" and (minetest.get_node(pointed_thing.under).name == "xpanes:bar_flat" or minetest.get_node(pointed_thing.under).name == "xpanes:bar")then
-				local pos = pointed_thing.under
-				for y = -2,2 do
-					pos.y=pos.y+y
-					if minetest.get_node(pos).name ==  "xpanes:bar_flat" or minetest.get_node(pos).name == "xpanes:bar" then
-						minetest.set_node(pos, {name = "air"})
-						minetest.add_particlespawner({
-							amount = 15,
-							time = 3,
-							collisiondetection = false,
-							collision_removal = false,
-							object_collision = false,
-							vertical = false,
-							texture = "blue_particle.png",
-							glow = 10,
-							minpos = {x=pos.x-0.5, y=pos.y-0.5, z=pos.z-0.5},
-							maxpos = {x=pos.x+0.5, y=pos.y+0.5, z=pos.z+0.5},
-							minvel = {x=0, y=0.1, z=0},
-							maxvel = {x=0, y=1, z=0},
-						})
-					end
-					pos.y=pos.y-y
-				end
-				
-				local inv = user:get_inventory()
-				local index = user:get_wield_index()
-				inv:set_stack("main",user:get_wield_index(),cc.."unlock_stone_active")
-				minetest.after(1, function(user, index)
-					inv:set_stack("main",index,cc.."unlock_stone")
-				end, user, index)
-			end
+		if pointed_thing.type == "node" and unbar(pointed_thing.under) then
+			local inv = user:get_inventory()
+			local index = user:get_wield_index()
+			inv:set_stack("main",user:get_wield_index(),cc.."unlock_stone_active")
+			minetest.after(1, function(user, index)
+				inv:set_stack("main",index,cc.."unlock_stone")
+			end, user, index)
 		end
 	end,
 })
